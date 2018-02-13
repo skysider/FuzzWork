17341acd6011854f515b9929a461c95340648cb5
skysider <junchaoluan@gmail.com>
2018-02-13 17:56:20 +0800

    add xpdf and binutils

---
+++ projects/binutils/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "update source"
+cd $SRC
+git pull origin
+cd ..
+
+echo "compile "
+BUILD_DIR=build
+if [ -d "$BUILD_DIR" ]; then
+    rm -rf $BUILD_DIR
+fi
+mkdir $BUILD_DIR
+cd $BUILD_DIR
+CC=afl-gcc CXX=afl-g++ $SRC/configure --disable-shared LIBS=-ldl LDFLAGS=-ldl --disable-64-bit-bfd
+AFL_USE_ASAN=1 make all-binutils
--- projects/xpdf/build.sh
+++ projects/xpdf/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 BUILD_DIR="build"
-echo "build xpdf with afl"
+echo "build xpdf with afl-gcc"
 if [ -d "$BUILD_DIR" ]; then
-    rm -rf build
+    rm -rf $BUILD_DIR
 fi 
 mkdir $BUILD_DIR
 cd $BUILD_DIR
@@ -10,11 +10,10 @@ export AFL_USE_ASAN=1
 cmake \
     -DCMAKE_DISABLE_FIND_PACKAGE_Qt5Widgets=1 \
     -DCMAKE_DISABLE_FIND_PACKAGE_Qt4=1 \
-    -DCMAKE_C_COMPILER=afl-clang \
-    -DCMAKE_CXX_COMPILER=afl-clang++ \
-    -DCMAKE_BUILD_TYPE=Debug \
-    -DCMAKE_C_COMPILER_DEBUG='-g -ggdb -O0 -fno-omit-frame-pointer' \
-    -DCMAKE_CXX_COMPILER_DEBUG='-g -ggdb -O0 -fno-omit-frame-poniter' \
+    -DCMAKE_C_COMPILER=afl-gcc \
+    -DCMAKE_CXX_COMPILER=afl-g++ \
+    -DCMAKE_C_FLAGS="-g -O0 -ggdb" \
+    -DCMAKE_CXX_FLAGS="-g -O0 -ggdb" \
     -DBUILD_SHARED_LIBS=OFF \
     --build ${SRC}
 make
