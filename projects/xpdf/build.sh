16372a381670224254be8c3d44a0a1406b19ccee
skysider <junchaoluan@gmail.com>
2018-02-13 11:22:51 +0800

    init

---
+++ projects/xpdf/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+BUILD_DIR="build"
+echo "build xpdf with afl"
+if [ -d "$BUILD_DIR" ]; then
+    rm -rf build
+fi 
+mkdir $BUILD_DIR
+cd $BUILD_DIR
+export AFL_USE_ASAN=1
+cmake \
+    -DCMAKE_DISABLE_FIND_PACKAGE_Qt5Widgets=1 \
+    -DCMAKE_DISABLE_FIND_PACKAGE_Qt4=1 \
+    -DCMAKE_C_COMPILER=afl-clang \
+    -DCMAKE_CXX_COMPILER=afl-clang++ \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DBUILD_SHARED_LIBS=OFF \
+    --build ${SRC_DIR}
+make
