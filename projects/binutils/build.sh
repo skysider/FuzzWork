b34f2fe7a3096d201a3fa393ecb4eb46cbe16ace
skysider <junchaoluan@gmail.com>
2018-02-28 11:25:51 +0800

    fix binutils

--- projects/binutils/build.sh
+++ projects/binutils/build.sh
@@ -11,5 +11,5 @@ if [ -d "$BUILD_DIR" ]; then
 fi
 mkdir $BUILD_DIR
 cd $BUILD_DIR
-CC=afl-gcc CXX=afl-g++ $SRC/configure --disable-shared LIBS=-ldl LDFLAGS=-ldl --disable-64-bit-bfd
+CC=afl-gcc CXX=afl-g++ $SRC/configure --disable-shared LIBS=-ldl LDFLAGS=-ldl --disable-64-bit-bfd CFLAGS="-g -O0" CXXFLAGS="-g -O0"
 AFL_USE_ASAN=1 make all-binutils
