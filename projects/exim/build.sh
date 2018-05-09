68d5ac192ba10519fe5ce0b57db4aa1306b5b699
skysider <junchaoluan@gmail.com>
2018-05-09 11:11:46 +0800

    exim

--- projects/exim/build.sh
+++ projects/exim/build.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
 cd exim
-git reset --hard cf3cd306062a08969c41a1cdd32c6855f1abecf1~1
 cd src
+rm -rf build-Linux-x86_64
 mkdir Local
 cp src/EDITME Local/Makefile
 cp exim_monitor/EDITME Local/eximon.conf
 sed -i 's/^EXIM_USER=/EXIM_USER=exim/' Local/Makefile
-sed -i '39 aecho\n' scripts/Configure-config.h
 useradd exim
-FULLECHO="" LFLAGS+="-fsanitize=address -lasan -pie" CFLAGS+="-fPIC -O1 -g -fsanitize=address" LDFLAGS+="-lasan -pie -ldl -lm -lcrypt" LIBS+="-lasan -pie" make -e exim
+CC=clang FULLECHO="" LFLAGS+="-fsanitize=address -pie" CFLAGS+="-fPIC -O1 -g -fno-omit-frame-pointer -fsanitize=address" LDFLAGS+="-pie -ldl -lm -lcrypt" LIBS+="-pie" make -e exim
 cd ../..
