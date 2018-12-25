a03f1ea8ba4f8cc1e68d95ef2de484bb2acfb68d
skysider <luanjunchao@nuaa.edu.cn>
2018-12-25 15:44:11 +0800

    Update build.sh

--- projects/binutils/build.sh
+++ projects/binutils/build.sh
@@ -13,3 +13,7 @@ mkdir $BUILD_DIR
 cd $BUILD_DIR
 CC=afl-gcc CXX=afl-g++ $SRC/configure --disable-shared LIBS=-ldl LDFLAGS=-ldl --disable-64-bit-bfd CFLAGS="-g -O0" CXXFLAGS="-g -O0"
 AFL_USE_ASAN=1 make all-binutils
+cd ../
+rm -rf build_normal && mkdir build_normal && cd build_normal
+$SRC/configure --disable-shared LIBS=-ldl LDFLAGS=-ldf --disable-64-bit-bfd CFLAGS="-g -O0" CXXFLAGS="-g -O0"
+make all-binutils
